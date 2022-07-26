44833b4ad186d7f32968bda14f52c1bf21963289
TingSyuanWang <vincent@fishboneapps.com>
2022-07-26 16:24:20 -0400

    solution: 35
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- Algorithms/0035.search-insert-position/SearchInsertPosition.java
+++ Algorithms/0035.search-insert-position/SearchInsertPosition.java
@@ -1,17 +1,11 @@
 class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
-        int l = 0, h = nums.length - 1;
-        int m = l + (h - l) / 2;
-        while (l <= h) {
-            m = l + (h - l) / 2;
-            if (nums[m] > target) {
-                h = m - 1;
-            } else if (nums[m] < target) {
-                l = m + 1;
-            } else {
-                return m;
-            }
+        int l = 0, h = nums.length;
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (nums[m] >= target) h = m;
+            else l = m + 1;
         }
-        return nums[m] > target ? m : m + 1;
+        return l;
     }
 }
